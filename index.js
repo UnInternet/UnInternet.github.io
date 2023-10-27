f487533d43104f10ac4388dd952c13a8b92f7e34
GabeTheCat123 <139125471+GabeTheCat123@users.noreply.github.com>
2023-10-27 16:00:20 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("btnSearch");
 
 button.onclick = function () {
-    var search = document.getElementById("textBoxEl").value;
+    var search = document.getElementById("text").value;
     window.open("https://uninternet.github.io/groups/" + search);
 }
